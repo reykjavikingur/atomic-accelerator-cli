741906a7d85bc3d33e8ade1984ba6304e90ca1e2
Andrew Strader <astrader@resourceammirati.com>
2018-04-07 18:22:59 -0400

    create sort and add functionality in stylesheet

--- test/stylesheet.spec.js
+++ test/stylesheet.spec.js
@@ -5,4 +5,110 @@ describe('Stylesheet', () => {
 	it('should exist', () => {
 		should(Stylesheet).be.ok();
 	});
+
+	describe('from empty string', () => {
+		var instance;
+		beforeEach(() => {
+			instance = Stylesheet.fromString('');
+		});
+		it('should be ok', () => {
+			should(instance).be.ok();
+		});
+		describe('.toString', () => {
+			it('should return empty string', () => {
+				should(instance.toString()).eql('');
+			});
+		});
+		describe('add path', () => {
+			beforeEach(() => {
+				instance.add('comp');
+			});
+			describe('.toString', () => {
+				it('should include comp', () => {
+					should(instance.toString()).eql(`@import 'comp';`);
+				});
+			})
+		});
+		describe('add invalid path', () => {
+			it('should throw error', () => {
+				should(() => {
+					instance.add('don\'t');
+				}).throw();
+			});
+		});
+	});
+
+	describe('from string with one import', () => {
+		var instance;
+		beforeEach(() => {
+			instance = Stylesheet.fromString(`@import 'foo';`);
+		});
+		describe('.toString', () => {
+			it('should return same as input', () => {
+				should(instance.toString()).eql(`@import 'foo';`);
+			})
+		});
+		describe('add other', () => {
+			beforeEach(() => {
+				instance.add('other');
+			});
+			describe('.toString', () => {
+				it('should include other', () => {
+					should(instance.toString()).eql(`@import 'foo';\n@import 'other';`);
+				});
+			});
+		});
+	});
+
+	describe('from string with two imports', () => {
+		var instance;
+		beforeEach(() => {
+			instance = Stylesheet.fromString(`@import 'foo';\n@import 'bar';`);
+		});
+		describe('.toString', () => {
+			it('should return same as input', () => {
+				should(instance.toString()).eql(`@import 'foo';\n@import 'bar';`);
+			})
+		});
+		describe('.sort', () => {
+			beforeEach(() => {
+				instance.sort();
+			});
+			describe('.toString', () => {
+				it('should return string with import statements sorted', () => {
+					should(instance.toString()).eql(`@import 'bar';\n@import 'foo';`);
+				});
+			});
+		});
+	});
+
+	describe('from string with extraneous line', () => {
+		var instance;
+		beforeEach(() => {
+			instance = Stylesheet.fromString(`.asdf {}`);
+		});
+		describe('.sort', () => {
+			beforeEach(() => {
+				instance.sort();
+			});
+			describe('.toString', () => {
+				it('should return empty string', () => {
+					should(instance.toString()).eql('');
+				});
+			});
+		});
+	});
+
+	describe('from string with import statements and extraneous lines', () => {
+		var instance;
+		beforeEach(() => {
+			instance = Stylesheet.fromString(`@import 'foo';\n.asdf{}\n@import 'bar';`);
+			instance.sort();
+		});
+		describe('.toString', () => {
+			it('should remove extraneous line', () => {
+				should(instance.toString()).eql(`@import 'bar';\n@import 'foo';`);
+			});
+		});
+	});
 });
